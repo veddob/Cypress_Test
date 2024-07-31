847413b909fa35a096c92f70a0f28c6efe900a78
Vedrana Dobric <veddob1@gmail.com>
2024-07-31 20:25:12 +0200

    Updated new-todo

--- cypress/e2e/1-getting-started/new-todo.cy.js
+++ cypress/e2e/1-getting-started/new-todo.cy.js
@@ -11,11 +11,37 @@
 // please read our getting started guide:
 // https://on.cypress.io/introduction-to-cypress
 
-describe("example to-do app", () => {
+describe("test isolation true", { testIsolation: true }, () => {
   beforeEach(() => {
     cy.visit("https://www.index.hr/");
+    cy.get("#didomi-notice-agree-button").click();
   });
+
+  it("Get first article", () => {
+    cy.get(".first-news a").first().click();
+  });
+});
+
+describe("test isolation false", { testIsolation: false }, () => {
+  it("Visit page", () => {
+    cy.clearAllCookies();
+    cy.clearAllLocalStorage();
+    cy.visit("https://www.index.hr/");
+  });
+
   it("Confirm cookies", () => {
     cy.get("#didomi-notice-agree-button").click();
+  });
+
+  it("Get first article", () => {
+    cy.get(".first-news a").first().click();
+  });
+
+  it("Get the last of most read articles", () => {
+    cy.get(".home-cube-big-timeline > ul > .most-read ").click();
+  });
+
+  it("Get Znanost from footer", () => {
+    cy.get("footer > .list-holder > ul > li > a").eq(6).click();
   });
 });
